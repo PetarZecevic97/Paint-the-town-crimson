0e4a6a95b97df06b0425dcba896d2567f655922b
AKile9V <akile9v@gmail.com>
2020-05-05 23:36:02 +0200

    faster spawing, pause 3sec between levels

--- Game/src/Entities/NPC/EnemiesFactory.cpp
+++ Game/src/Entities/NPC/EnemiesFactory.cpp
@@ -28,30 +28,17 @@ namespace Game
 	void EnemiesFactory::Update(float dt, Engine::EntityManager* entityManager, Engine::TextureManager* texture)
 	{
 		m_Delta += dt;
+		bool exit = m_CurrentElement <= m_Levels[m_CurrentLevel];
 
-		if (m_Delta > 2.f && m_CurrentElement != MAX_ENEMIES)
+		if (m_Delta > m_SpawnTimer && exit)
 		{
-			m_Delta = 0.f;
-			int randomPosition = rand() % 4;
-			int enemyType = rand() % 4;
-			switch (enemyType)
-			{
-			case 0:
-				m_WaterNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
-				break;
-			case 1:
-				m_FireNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
-				break;
-			case 2:
-				m_WindNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
-				break;
-			case 3:
-				m_EarthNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
-				break;
-			default:
-				ASSERT(randomPosition>3 || randomPosition<0, "Out of range: [0,3]");
-			}
-			m_CurrentElement++;
+			SpawnNextNPC(entityManager, texture);
+		}
+		if (!exit && entityManager->GetAllEntitiesWithComponent<EnemyComponent>().size() == 0)
+		{
+			m_Pause = true;
+			Reset();
+			m_SleepTimeStart = SDL_GetTicks();
 		}
 
 		m_WaterNPCController->Update(dt,entityManager);
@@ -63,7 +50,11 @@ namespace Game
 
 	void EnemiesFactory::Reset()
 	{
-
+		m_CurrentElement = 1;
+		m_CurrentLevel++;
+		m_Delta = 0;
+		m_SpawnTimer = 2.f;
+		m_SpawnFrequency--;
 	}
 
 	bool EnemiesFactory::ShutDown()
@@ -71,4 +62,37 @@ namespace Game
 		return true;
 	}
 
+	void EnemiesFactory::Sleep()
+	{
+		if (SDL_GetTicks() - m_SleepTimeStart > 3000 && m_CurrentLevel<3)
+		{
+			m_Pause = false;
+		}
+	}
+
+	void EnemiesFactory::SpawnNextNPC(Engine::EntityManager* entityManager, Engine::TextureManager* texture)
+	{
+		m_SpawnTimer -= m_CurrentElement % m_SpawnFrequency == 0 ? 0.2f : 0.f;
+		m_Delta = 0.f;
+		int randomPosition = rand() % 4;
+		int enemyType = rand() % 4;
+		switch (enemyType)
+		{
+		case 0:
+			m_WaterNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
+			break;
+		case 1:
+			m_FireNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
+			break;
+		case 2:
+			m_WindNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
+			break;
+		case 3:
+			m_EarthNPCController->Init(entityManager, texture->GetTexture("enemy"), m_SpawnPositions[randomPosition]);
+			break;
+		default:
+			ASSERT(randomPosition > 3 || randomPosition < 0, "Out of range: [0,3]");
+		}
+		m_CurrentElement++;
+	}
 }
