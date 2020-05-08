38a8fb7a0db7f0c87c668be481d3c83ab9f2a6a1
Vladimir <vladimir@wings.rs>
2020-05-08 02:35:16 +0200

    Stage Advance implemented

--- Game/src/Entities/StageController.cpp
+++ Game/src/Entities/StageController.cpp
@@ -18,6 +18,7 @@ namespace Game
 
         stage->AddComponent<Engine::TransformComponent>(60, 0, 1152.f, 720.f);
         stage->AddComponent<Engine::ObstacleComponent>();
+        stage->AddComponent<Engine::LevelComponent>();
 
         entityManager_->AddEntity(std::move(stage));
         m_currentLevelNo = LevelNumber::LEVEL_ONE;
@@ -27,9 +28,32 @@ namespace Game
 
     void StageController::Update(Engine::EntityManager* entityManager_, int window_width, int window_height,bool isGameOver_) 
     {
-        if(isGameOver_)m_currentLevelNo = LevelNumber::LEVEL_GAME_OVER;
-        if (m_currentLevelNo == LevelNumber::LEVEL_ONE)m_currentLevelNo = LevelNumber::LEVEL_TWO;
-        else if (m_currentLevelNo == LevelNumber::LEVEL_TWO)m_currentLevelNo = LevelNumber::LEVEL_THREE;
-        else if (m_currentLevelNo == LevelNumber::LEVEL_THREE)m_currentLevelNo = LevelNumber::LEVEL_WIN;
+        auto stage = entityManager_->GetAllEntitiesWithComponent< Engine::LevelComponent>()[0];
+        auto* sprite = stage->GetComponent<Engine::SpriteComponent>();
+
+        if (isGameOver_)
+        {
+            SDL_Rect new_rect = { 0, 720*3, 1240, 720 };
+            sprite->m_src = new_rect;
+            LevelNumber::LEVEL_GAME_OVER;
+        }
+        else if (m_currentLevelNo == LevelNumber::LEVEL_ONE) 
+        {
+            SDL_Rect new_rect = { 0, 720, 1240, 720 };
+            sprite->m_src = new_rect;
+            m_currentLevelNo = LevelNumber::LEVEL_TWO; 
+        }
+        else if (m_currentLevelNo == LevelNumber::LEVEL_TWO)
+        {
+            SDL_Rect new_rect = { 0, 720*2, 1240, 720 };
+            sprite->m_src = new_rect;
+            m_currentLevelNo = LevelNumber::LEVEL_THREE;
+        }
+        else if (m_currentLevelNo == LevelNumber::LEVEL_THREE) 
+        { 
+            SDL_Rect new_rect = { 0, 720*4, 1240, 720 };
+            sprite->m_src = new_rect;
+            m_currentLevelNo = LevelNumber::LEVEL_WIN;
+        }
     }
 }
